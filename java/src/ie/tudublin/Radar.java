36bed89c53dbba87bae79391b4483169d110c6c4
skooter500 <skooter500@gmail.com>
2019-03-21 11:14:12 +0000

    bla

--- java/src/ie/tudublin/Radar.java
+++ java/src/ie/tudublin/Radar.java
@@ -21,7 +21,7 @@ public class Radar
 
     public void render()
     {
-        ui.pushMatrix();        
+        /*ui.pushMatrix();        
         ui.noFill();
         ui.stroke(0, 200, 0);
         ui.translate(pos.x, pos.y);
@@ -29,6 +29,13 @@ public class Radar
         ui.ellipse(0, 0, radius * 2, radius * 2);
         ui.line(0,0,0,-radius);
         ui.popMatrix();
+        */
+
+        ui.noFill();
+        ui.ellipse(pos.x, pos.y, radius * 2, radius * 2);
+        float x2 = pos.x + (float) Math.sin(theta) * radius;
+        float y2 = pos.y - (float) Math.cos(theta) * radius;
+        ui.line(pos.x, pos.y, x2, y2);
     }
 
     float timeDelta = 1.0f / 60.0f;
