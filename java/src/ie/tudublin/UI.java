8f5fee54af6d6253030d429f4765a4e9bfe12053
skooter500 <skooter500@gmail.com>
2019-03-21 11:15:01 +0000

    not full screen

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -27,9 +27,9 @@ public class UI extends PApplet
 
     public void settings()
     {
-        //size(800, 800);
+        size(800, 800);
         // Use fullscreen instead of size to make your interface fullscreen
-        fullScreen(P3D); 
+        //fullScreen(P3D); 
     }
 
     public void setup()
